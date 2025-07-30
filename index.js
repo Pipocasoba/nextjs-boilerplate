a3854e1fb4c27df8e7c211ceb2b9003483044903
Pipocasoba <pipocasoba@gmail.com>
2025-07-30 15:36:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Arquivo de entrada do site OBÁ Pipocas
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-yellow-100 text-center">
+      <h1 className="text-4xl font-bold text-red-600">OBÁ Pipocas</h1>
+      <p className="mt-4 text-lg text-gray-700">Milho sagrado, pipoca divina</p>
+    </div>
+  )
+}
